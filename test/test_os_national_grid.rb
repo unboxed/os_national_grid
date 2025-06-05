d4626ec94be0068c86a542addf9dfaf4e8eca708
Evangelos Giataganas <e.giataganas@gmail.com>
2025-06-05 16:44:51 +0300

    Migrating os_national_grid service from BOPS

--- test/test_os_national_grid.rb
+++ test/test_os_national_grid.rb
@@ -3,11 +3,24 @@
 require "test_helper"
 
 class TestOsNationalGrid < Minitest::Test
-  def test_that_it_has_a_version_number
-    refute_nil ::OsNationalGrid::VERSION
+  def setup
+    @easting = 481_987.066
+    @northing = 213_552.27
+    @longitude = -0.812036
+    @latitude = 51.814605
   end
 
-  def test_it_does_something_useful
-    assert false
+  def test_os_ng_to_wgs84
+    lng, lat = OsNationalGrid.os_ng_to_wgs84(@easting, @northing)
+
+    assert_in_delta @longitude, lng, 0.002, "Longitude not within tolerance"
+    assert_in_delta @latitude, lat, 0.002, "Latitude not within tolerance"
+  end
+
+  def test_wgs84_to_os_ng
+    easting, northing = OsNationalGrid.wgs84_to_os_ng(@longitude, @latitude)
+
+    assert_in_delta @easting, easting, 2.0, "Easting not within tolerance"
+    assert_in_delta @northing, northing, 2.0, "Northing not within tolerance"
   end
 end
